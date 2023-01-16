ec34b34f616e8803a75fc5260fe57a38ca9b01ad
Winston Muijs <winstonhmuijs@gmail.com>
2023-01-16 17:32:40 +0100

    Updated version of Spaceship Class
    Added methods for updating, timer and laser, also positioning spaceship with the mouse.

--- main.py
+++ main.py
@@ -8,11 +8,38 @@ class Spaceship(pygame.sprite.Sprite):
 
         # 1. init parent class sprite.Sprite
         super().__init__(groups)
-        # 2. making a surface --> image
+        # 2. making a surface --> images
         self.image = pygame.image.load("./graphics/ship.png").convert_alpha()
         # 3. making a rectangle
         self.rect = self.image.get_rect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 
+        # timer
+        self.can_shoot = True
+        self.shoot_time = None
+
+    # laser timer
+    def laser_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.shoot_time > 500:
+                self.can_shoot = True
+    # positioning spaceship with mouse
+    def position(self):
+        pos = pygame.mouse.get_pos()
+        self.rect.center = pos
+
+    # laser shooting
+    def laser_shoot(self):
+        if pygame.mouse.get_pressed()[0] and self.can_shoot:
+            print('shoot laser')
+            self.can_shoot = False
+            self.shoot_time = pygame.time.get_ticks()
+
+    # Creates lots of other methods within update and then in de game loop
+    def update(self):
+        self.laser_timer()
+        self.position()
+        self.laser_shoot()
 
 class Laser(pygame.sprite.Sprite):
     def __init__(self, groups, pos):
@@ -49,9 +76,10 @@ clock = pygame.time.Clock()
 background = pygame.image.load("./graphics/background.png").convert()
 
 # Sprite Group gives a object
-spaceship_group = pygame.sprite.Group()
+
 laser_group = pygame.sprite.Group()
 asteroids_group = pygame.sprite.Group()
+spaceship_group = pygame.sprite.Group()
 
 # create objects of Sprite Class
 # Not yet visible because the sprite has tobe put into a group
@@ -72,9 +100,15 @@ while True:
 
     # drawing background
     screen.blit(background, (0, 0))
+
+    # update groups
+    spaceship.update()
+
     # images : draws images on screen
-    spaceship_group.draw(screen)
+
     laser_group.draw(screen)
     asteroids_group.draw(screen)
+    spaceship_group.draw(screen)
+
     # draw screen
     pygame.display.update()
